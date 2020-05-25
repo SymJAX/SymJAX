f00fa44bf6d48c7d82595f7f7a38a8267787a0c3
RandallBalestriero <randallbalestriero@gmail.com>
2020-05-25 18:56:28 -0500

    adding the control flow

--- examples/control_flow.py
+++ examples/control_flow.py
@@ -3,39 +3,28 @@ import numpy as np
 import sys
 sys.path.insert(0, "../")
 
-import theanoxla
-import theanoxla.tensor as T
-import theanoxla.nn as nn
+import symjax
+import symjax.tensor as T
 
 
+# map
+xx = T.ones(10)
+a = T.map(lambda a: a * 2, xx)
+g = symjax.gradients(a.sum(),xx)[0]
+f = symjax.function(outputs=[a,g])
 
-w = T.Placeholder((3,), np.float32, name='w')
+# scan
+xx = T.ones(10) * 2
+a = T.scan(lambda c, x: (c*x,c*x), T.ones(1), xx)
+g = symjax.gradients(a[1][-1],xx)[0]
+f = symjax.function(outputs=[a,g])
 
-# MAP example 1
-output = T.map(lambda a, b: T.pow(a, b), w, T.cast(T.arange(3), 'float32'))
-print(output.get({w: jax.numpy.arange(3).astype('float32')}))
-fn = theanoxla.function(w, outputs=[output])
-print(fn(jax.numpy.arange(3).astype('float32')))
-
-# MAP example 2
-output = T.map(lambda a: T.pow(a, 2.), T.cast(T.arange(3), 'float32'))
-print(output.get())
-fn = theanoxla.function(outputs=[output])
-print(fn())
-
-
-# SCAN example 1
-output = T.scan(lambda a, b: a + b, T.zeros(1), T.reshape(w, (3, 1)))
-print(output.get({w: jax.numpy.arange(3).astype('float32')}))
-fn = theanoxla.function(w, outputs=[output])
-print(fn(jax.numpy.arange(3).astype('float32')))
-
-# SCAN example 2
-output = T.scan(lambda a, b, c: a + b * c, T.zeros(1),
-                T.cast(T.arange(3), 'float32'), T.cast(T.arange(3), 'float32'))
-print(output.get())
-fn = theanoxla.function(outputs=[output])
-print(fn())
+# fori loop
+b= T.Placeholder((), 'int32')
+xx = T.ones(1)
+a = T.fori_loop(0, b, lambda i,x:i*x, xx)
+f = symjax.function(b,outputs=a)
+print(f(0), f(1),f(2), f(3))
 
 
 # COND example 1
@@ -44,7 +33,7 @@ output = T.cond(value < 0, (value, w), lambda a, b: a * b, (value, w),
                 lambda a, b: a*b)
 print(output.get({value: -1., w: jax.numpy.arange(3).astype('float32')}))
 print(output.get({value: 1., w: jax.numpy.arange(3).astype('float32')}))
-fn = theanoxla.function(value, w, outputs=[output])
+fn = symjax.function(value, w, outputs=[output])
 print(fn(-1., jax.numpy.arange(3).astype('float32')))
 print(fn(1., jax.numpy.arange(3).astype('float32')))
 
@@ -54,7 +43,7 @@ output = T.cond(value < 0, value, lambda a: a * 10, value,
                 lambda a: a * 20)
 print(output.get({value: -1.}))
 print(output.get({value: 1.}))
-fn = theanoxla.function(value, outputs=[output])
+fn = symjax.function(value, outputs=[output])
 print(fn(-1.))
 print(fn(1.))
 
--- symjax/tensor/control_flow.py
+++ symjax/tensor/control_flow.py
@@ -1,73 +1,9 @@
-import jax.numpy as jnp
 import jax.lax as jla
-from .base import Op, Tensor, symjax_to_jax_fn
-from .base import Tensor, jax_wrap
-import numpy
+from .base import jax_wrap
 
+cond = jax_wrap(jla.cond)
+fori_loop = jax_wrap(jla.fori_loop)
+while_loop = jax_wrap(jla.while_loop)
+scan = jax_wrap(jla.scan)
+map = jax_wrap(jla.map)
 
-
-
-def _scan(fn, init, xs, constants=(), length=None):
-    def new_fn(*args):
-        return fn(*args, *constants)
-    return jla.scan(new_fn, init, xs, length)
-        
-_scan2= jax_wrap(_scan)
-
-def scan(fn, init, xs, constants=(), length=None):
-    newfn = symjax_to_jax_fn(fn)
-    return _scan2(newfn, init, xs, constants, length)
-
-scan.__doc__ = jla.scan.__doc__
-
-#_cond = Op(jla.cond, name='cond')
-
-def _cond(predicate, true_predicate, true_fun, false_predicate, false_fun):
-        """ predicate should be a boolean tensor with shape ()
-        true_input is the input passed to true_fn that will give the output
-        if the predicate evaluates to True, and conversely for False..."""
-
-        # in case the given predicates are not tuples, set them
-        if type(true_predicate) != tuple:
-            if type(true_predicate) == list:
-                true_predicate = tuple(true_predicate)
-            else:
-                true_predicate = (true_predicate, )
-        if type(false_predicate) != tuple:
-            if type(false_predicate) == list:
-                false_predicate = tuple(false_predicate)
-            else:
-                false_predicate = (false_predicate, )
-
-        newtruefn = lambda x, _fn=true_fun: symjax_to_jax_fn(*x, _fn=_fn)
-        newfalsefn = lambda x, _fn=false_fun: symjax_to_jax_fn(*x, _fn=_fn)
-        return jla.cond(predicate, true_predicate, newtruefn, false_predicate,
-                   newfalsefn)
-
-cond = jax_wrap(_cond, doc_func=_cond)
-
-##class cond(Op):
-##    @staticmethod
-##    def fn(predicate, true_predicate, true_fun, false_predicate, false_fun):
-##        """ predicate should be a boolean tensor with shape ()
-##        true_input is the input passed to true_fn that will give the output
-##        if the predicate evaluates to True, and conversely for False..."""
-##
-##        # in case the given predicates are not tuples, set them
-##        if type(true_predicate) != tuple:
-##            if type(true_predicate) == list:
-##                true_predicate = tuple(true_predicate)
-##            else:
-##                true_predicate = (true_predicate, )
-##        if type(false_predicate) != tuple:
-##            if type(false_predicate) == list:
-##                false_predicate = tuple(false_predicate)
-##            else:
-##                false_predicate = (false_predicate, )
-##
-##        newtruefn = lambda x, _fn=true_fun: symjax_to_jax_fn(*x, _fn=_fn)
-##        newfalsefn = lambda x, _fn=false_fun: symjax_to_jax_fn(*x, _fn=_fn)
-##        return jla.cond(predicate, true_predicate, newtruefn, false_predicate,
-##                   newfalsefn)
-##
-#
