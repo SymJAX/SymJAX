3ce88b279dd18038cc2138b964a89157b945529a
RandallBalestriero <randallbalestriero@gmail.com>
2020-05-28 11:41:25 -0500

    added permutation random

--- symjax/tensor/random.py
+++ symjax/tensor/random.py
@@ -4,12 +4,12 @@ import sys
 
 
 
-_RANDOM_FUNCTIONS = [jnp.bernoulli, jnp.beta, jnp.cauchy,
-                     jnp.dirichlet, jnp.gamma, jnp.gumbel,
-                     jnp.laplace, jnp.logit, jnp.categorical,
+_RANDOM_FUNCTIONS = [jnp.bernoulli, jnp.beta, jnp.categorical, jnp.cauchy,
+                     jnp.dirichlet, jnp.exponential, jnp.gamma, jnp.gumbel,
+                     jnp.laplace, jnp.logistic,
                      jnp.multivariate_normal, jnp.normal,
-                     jnp.pareto, jnp.randint, jnp.shuffle,
-                     jnp.threefry_2x32, jnp.truncated_normal,
+                     jnp.pareto, jnp.permutation, jnp.poisson, jnp.randint,
+                     jnp.shuffle, jnp.t, jnp.threefry_2x32, jnp.truncated_normal,
                      jnp.uniform]
 
 
