b3716078c1b310453c63e627ec5e6ecc9b6b6fc3
RandallBalestriero <randallbalestriero@gmail.com>
2020-06-08 14:18:43 -0500

    added pytest

---
+++ tests/base_test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__      = "Randall Balestriero"
+
+
+import symjax
+import numpy
+
+def test_add():
+    a = symjax.tensor.ones(2)
+    assert symjax.tensor.get(a.max()) == 1
