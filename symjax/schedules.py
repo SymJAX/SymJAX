8922357fbfec8166bfbc46bffdb55aafa435b419
RandallBalestriero <randallbalestriero@gmail.com>
2020-06-20 21:21:21 -0500

    tensor

--- symjax/schedules.py
+++ symjax/schedules.py
@@ -26,7 +26,8 @@ def piecewise_constant(init, values, step=None):
     step count the number of steps
     """
     if step is None:
-        step = Variable(0, trainable=False, name='PiecewiseConstant_step')
+        step = tensor.Variable(
+            0, trainable=False, name='PiecewiseConstant_step')
     keys, values = list(values.keys()), list(values.values())
     keys.insert(0, 0)
     values.insert(0, init)
@@ -35,8 +36,10 @@ def piecewise_constant(init, values, step=None):
     arg = numpy.argsort(keys)
     keys, values = keys[arg], values[arg]
     index = (step < keys).argmax()
-    v = Variable(values, trainable=False, name='PiecewiseConstant_values')
+    v = tensor.Variable(values, trainable=False,
+                        name='PiecewiseConstant_values')
     return T.dynamic_slice_in_dim(v, index - 1, 1, 0), step
+
 
 class PiecewiseConstant(Schedule):
 
@@ -46,7 +49,7 @@ class PiecewiseConstant(Schedule):
         self.step = tensor.Variable(
             0, trainable=False, name='piecewise_constant_step', dtype='float32')
         self.value = piecewise_constant(self.init, self.values,
-                                              self.step)[0]
+                                        self.step)[0]
         self.updates = {self.step: self.step + 1}
         self.variables = [self.step]
 
@@ -68,20 +71,18 @@ class Linear(Schedule):
         return self.value
 
 
-
-
-
-
 def ExponentialMovingAverage(value, alpha, step=None, init=None):
     if step is None:
-        _step = Variable(0, trainable=False, name='step', dtype='float32')
+        _step = tensor.Variable(
+            0, trainable=False, name='step', dtype='float32')
     else:
         _step = step
     if init is None:
-        var = Variable(numpy.zeros(value.shape), trainable=False,
-                       name='EMA', dtype='float32')
+        var = tensor.Variable(numpy.zeros(value.shape), trainable=False,
+                              name='EMA', dtype='float32')
     else:
-        var = Variable(init, trainable=False, name='EMA', dtype='float32')
+        var = tensor.Variable(init, trainable=False,
+                              name='EMA', dtype='float32')
 
     new_value = T.where(T.equal(_step, 0), value,
                         var * alpha + (1 - alpha) * value)
@@ -92,13 +93,12 @@ def ExponentialMovingAverage(value, alpha, step=None, init=None):
     return var, updates, _step
 
 
-
 class Exponential(Schedule):
 
     def __init__(self, init, slope):
         self.init = init
         self.slope = slope
-        self.value = tensor.Variable( init, trainable=False,
+        self.value = tensor.Variable(init, trainable=False,
                                      name='exponential_variable', dtype='float32')
         self.updates = {self.value: self.value * slope}
         self.variables = [self.value]
