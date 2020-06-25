eef28fe844ab3b330aadd75a23556d074a87db2c
Brandon T. Willard <brandonwillard@users.noreply.github.com>
2020-06-25 09:47:10 -0500

    Remove Python build artifacts

--- build/lib/symjax/datasets/brain_mnist.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-__author__      = "Randall Balestriero"
-#http://mindbigdata.com/opendb/index.html
--- build/lib/symjax/datasets/high_gamma.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-__author__      = "Randall Balestriero"
-#https://github.com/robintibor/high-gamma-dataset
